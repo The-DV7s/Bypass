e563072fc478e2c03552934b7f9c76a5f59afbfe
Flareon <167229955+xyzkyle@users.noreply.github.com>
2024-05-06 17:08:39 +0500

    Create bypass.lua

---
+++ bypass/a/b/c/d/e/f/g/h/i/j/k/l/m/n/o/p/q/r/s/t/u/v/w/x/y/z/bypass.lua
@@ -0,0 +1,22 @@
+--Copyright claim FLarex Inc
+--Obfuscated by Rvxfy (zvryx0#0) 
+local function rvx_Hard()
+  os.exit()
+end
+local rvx_Hardifyed = {" WHY ARE U HERE SKID "}
+1010101111111111111111111110000000(-) rvx_Hard()
+if rvx_Hard == nil then print(error)
+end
+return(function(e)local e=2;local function error() string.byte;end(e)(...)
+    return "e",e
+    local function r_eturn_function()
+      io.open()
+    end
+    if e == false then rvx_Hard()
+      is={}
+      v={}
+      if_={}
+      if v.Code == is.Minified then print("Code is minified.. Glad you did not break this")
+      end
+    end
+  end
